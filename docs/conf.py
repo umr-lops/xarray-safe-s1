8530db199218ec132402fc307064583d7e4a1080
yreynaud <yann.reynaud10@gmail.com>
2023-05-03 10:15:14 +0200

    Change docstring type in documentation config

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,20 @@ exclude_patterns = ["_build", "directory"]
 # nitpicky mode: complain if references could not be found
 nitpicky = True
 
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_type_aliases = None
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
